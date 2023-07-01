784adb3ad4729a6c45c38dfa3c9f466111f6376f
carlos stekel <carlosstekel@gmail.com>
2023-07-01 18:02:14 -0400

    progreso

--- complement.py
+++ complement.py
@@ -2,13 +2,10 @@ import numpy as np
 import random as rd
 import pandas as pd
 
-
-max_length=50
-k=3
 primo= 104723
 
 #Definiremos Una matriz de tamañp k x max_length con k arrreglos de tamño max_length con valores aleatorios en [1,p-1]
-def genera_matriz_a():
+def genera_matriz_a(k, max_length):
     a= np.zeros((k,max_length))
     for i in range(k):
         for j in range(max_length):
@@ -17,8 +14,8 @@ def genera_matriz_a():
     return a
 
 
-#Definiremos un arreglo b de largo max_lenght que posee valores aleatorios entre [0,p-1]
-def genera_arreglo_b():
+#Definiremos un arreglo b de largo k que posee valores aleatorios entre [0,p-1]
+def genera_arreglo_b(k):
     b=np.zeros(k)
     for i in range(len(b)):
         b[i]=rd.randint(0,primo-1)
@@ -33,11 +30,7 @@ def sacar_porcentaje_de_datos(data1,data2, porcentaje,total_datos):
     cantidad_restante= total_datos-cantidad_a_sacar
     filas_aleatorias_df1 = df1.sample(cantidad_a_sacar)
     filas_aleatorias_df2=df2.sample(cantidad_restante)
-    print(filas_aleatorias_df1.count())
-    print(filas_aleatorias_df2.count())
     filas_aleatorias = pd.concat([filas_aleatorias_df1, filas_aleatorias_df2])
     data_final = filas_aleatorias["Name"].to_numpy()
     np.random.shuffle(data_final)
     return(data_final)
-
-#print(sacar_porcentaje_de_datos("Popular-Baby-Names-Final.csv","Films-Actualizado.csv",50,1000))
--- main.py
+++ main.py
@@ -4,7 +4,16 @@ import numpy as np
 import random as rd
 import pandas as pd
 import math
-from complement import genera_arreglo_b, genera_matriz_a, genera_arrelo_busqueda
+from complement import genera_arreglo_b, genera_matriz_a, sacar_porcentaje_de_datos
+
+def hash(string, a, b, m): #funcion de hash
+    ascii_lista = [ord(caracter) for caracter in string]
+    x = 0
+    for i in range(len(ascii_lista)):
+        x += ascii_lista[i]*a[i]
+    x += b
+    x = (x % primo) % m
+    return int(x)
 
 # create a dataframe after reading .csv file
 dataframe = pd.read_csv('Popular-Baby-Names-Final.csv') 
@@ -20,4 +29,22 @@ for e in [0.25, 0.1, 0.05, 0.01]: #probabilidades de error
     m = math.ceil(-D*math.log(e) / (math.log(2)**2))
     k = round(m/D * math.log(2))
     print("Se usara un arreglo de tamaño " + str(m) + " con " + str(k) + " funciones de hash")
-    
+
+    M = np.zeros(m)
+    a = genera_matriz_a(k, maxlen)
+    b = genera_arreglo_b(k)
+
+    print("Inicio de ingresar Popular-Baby-Names-Final.csv")
+    #read csv, and split on "," the line
+    csv_file = csv.reader(open('Popular-Baby-Names-Final.csv', "r"), delimiter=",")
+    #loop through the csv list
+    for row in csv_file:
+        nombre = row[0]
+        for i in range(k):
+            r = hash(nombre, a[i], b[i], m)
+            M[r] = 1
+    print("Se ingreso Popular-Baby-Names-Final.csv al filtro")
+        
+    for p in [80, 60, 40, 20]:
+        buscar = sacar_porcentaje_de_datos('Popular-Baby-Names-Final.csv', 'Films-Actualizado.csv', p, n)
+        print("Se genero una busqueda con " + str(p) + "%" + " de exito en busquedas")
