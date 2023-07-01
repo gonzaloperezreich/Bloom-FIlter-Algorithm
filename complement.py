b32895b2ac7188bb86e835f1d8ed64f3ffeb27e0
carlos stekel <carlosstekel@gmail.com>
2023-07-01 16:35:01 -0400

    cosas

--- complement.py
+++ complement.py
@@ -1,5 +1,9 @@
 import numpy as np
 import random as rd
+import csv
+import pandas as pd
+import sys
+
 max_length=50
 k=3
 primo= 104723
@@ -22,8 +26,35 @@ def genera_arreglo_b():
     
     return b
 
+def sample_n_from_csv(filename:str, n:int=100, total_rows:int=None) -> pd.DataFrame:
+    if total_rows==None:
+        with open(filename,"r") as fh:
+            total_rows = sum(1 for row in fh)
+    if(n>total_rows):
+        print("Error: n > total_rows", file=sys.stderr) 
+    skip_rows =  rd.sample(range(1,total_rows+1), total_rows-n)
+    return pd.read_csv(filename, skiprows=skip_rows)
 
-a= genera_matriz_a()
-b=genera_arreglo_b()
-print(a)
-print(b)
+# Funcion que enera un arreglo de largo n con un porcentaje de elementos que estan en el csv de a
+# a = [0, 1]
+def genera_arrelo_busqueda(n, a):
+    #arr = np.empty(n, dtype=object)
+    datos = []
+    elementoscsv = int(n*a)
+    print(elementoscsv)
+    #read csv, and split on "," the line
+    dft = pd.read_csv('Popular-Baby-Names-Final.csv')
+    dff = pd.read_csv('Films-Actualizado.csv')
+
+    names = sample_n_from_csv('Popular-Baby-Names-Final.csv', n=elementoscsv)
+#    test = sample_n_from_csv('Films-Actualizado.csv', n=(n - elementoscsv))
+
+    for name in names:
+        datos.append(name[0])
+#    for s in test:
+#        datos.append(s)
+
+    return datos
+
+print(sample_n_from_csv('Popular-Baby-Names-Final.csv', n=5))
+print(genera_arrelo_busqueda(10, 0.5))
