d29b4fd2f9ac8d72b1e0cd5c1bea4ccecae41440
gonzaloperezreich <gonzaloperezreich@gmail.com>
2023-07-05 17:21:46 -0400

    arreglando cosas

--- main.py
+++ main.py
@@ -21,115 +21,18 @@ def hash(string, a, b, m): #funcion de hash
 
 # create a dataframe after reading .csv file
 dataframe = pd.read_csv('Popular-Baby-Names-Final.csv') 
-n = 200 #tamaño de los test
+n = 1000 #tamaño de los test
 D = dataframe.shape[0] #tamaño del csv
 maxlen = 50 #solo dejamos los strings hasta con 50 caracteres
 # primo= 10000019,10000003, 100003, 
-primo = 100019
-
-'''
-print("Probando m óptimo")
-for e in [0.25, 0.1, 0.05, 0.01, 0.001]: #probabilidades de error
-    print("\n\nEmpezando test con error " + str(e))
-    # Calcular el tamaño del arregglo m y la cantidad de funciones k
-    mopt = math.ceil(-D*math.log(e) / (math.log(2)**2))
-    print("el m óptimo es ", mopt)
-    m = max(mopt-200000, 1)
-    for m in range(m, mopt+300000, 100000):
-        k = math.ceil( (m/D) * math.log(2))
-        print("Se usara un arreglo de tamaño " + str(m) + " con " + str(k) + " funciones de hash")
-
-        M = bitarray.bitarray(m)
-        M.extend([0] * m)
-        #M = np.zeros(m)
-        a = genera_matriz_a(k)
-        b = genera_arreglo_b(k)
-
-        print("Inicio de ingresar Popular-Baby-Names-Final.csv")
-        #read csv, and split on "," the line
-        csv_file = csv.reader(open('Popular-Baby-Names-Final.csv', "r"), delimiter=",")
-        #loop through the csv list
-        for row in csv_file:
-            nombre = row[0]
-            for i in range(k):
-                r = hash(nombre, a[i], b[i], m)
-                M[r] = 1
-        print("Se ingreso Popular-Baby-Names-Final.csv al filtro")
-
-        for p in [80, 60, 40, 20]:
-            buscar = sacar_porcentaje_de_datos('Popular-Baby-Names-Final.csv', 'Films-Actualizado.csv', p, n)
-            print("\nSe genero una busqueda con " + str(p) + "%" + " de exito en busquedas")
-            print("Inicio de busqueda con filtro")
-            inicio1 = time.time()
-            for s in buscar:
-                for i in range(k):
-                    r = hash(s, a[i], b[i], m)
-                    if M[r] == 0:
-                        break
-                #Si nunca hizo break, el algoritmo dice que lo encontró (podría ser FP)
-            fin1 = time.time()
-            print("La busqueda con filtro demoro " + str(fin1-inicio1) + "s ")
-'''
-
-'''
-for e in [0.25, 0.1, 0.05, 0.01, 0.001]: #probabilidades de error
-    print("\n\nEmpezando test con error " + str(e))
-    # Calcular el tamaño del arregglo m y la cantidad de funciones k
-    m = math.ceil(-D*math.log(e) / (math.log(2)**2))
-    k = round( (m/D) * math.log(2))
-    print("Se usara un arreglo de tamaño " + str(m) + " con " + str(k) + " funciones de hash")
-
-    M = bitarray.bitarray(m)
-    M.extend([0] * m)
-    #M = np.zeros(m)
-    a = genera_matriz_a(k, maxlen)
-    b = genera_arreglo_b(k)
-
-    print("Inicio de ingresar Popular-Baby-Names-Final.csv")
-    #read csv, and split on "," the line
-    csv_file = csv.reader(open('Popular-Baby-Names-Final.csv', "r"), delimiter=",")
-    #loop through the csv list
-    for row in csv_file:
-        nombre = row[0]
-        for i in range(k):
-            r = hash(nombre, a[i], b[i], m)
-            M[r] = 1
-    print("Se ingreso Popular-Baby-Names-Final.csv al filtro")
-
-    # for i in range(len(M)):
-        #print(M[i])
-
-    for p in [80, 60, 40, 20]:
-        buscar = sacar_porcentaje_de_datos('Popular-Baby-Names-Final.csv', 'Films-Actualizado.csv', p, n)
-        print("\nSe genero una busqueda con " + str(p) + "%" + " de exito en busquedas")
-        print("Inicio de busqueda con filtro")
-        inicio1 = time.time()
-        for s in buscar:
-            for i in range(k):
-                r = hash(s, a[i], b[i], m)
-                if M[r] != 1:
-                    break
-            #Si nunca hizo break, el algoritmo dice que lo encontró (podría ser FP)
-        fin1 = time.time()
-        print("La busqueda con filtro demoro " + str(fin1-inicio1) + "s ")
-        print("Inicio de busqueda sin filtro")
-        inicio2 = time.time()
-        for s in buscar:
-            for row in csv_file:
-                print(s)
-                print(row[0])
-                if s == row[0]:
-                    break #Si hace break, lo encontró
-        fin2 = time.time()
-        print("La busqueda sin filtro demoro " + str(fin2-inicio2) + "s ")
-'''
-
+primo = 100000019
 
 for e in [0.25, 0.1, 0.05, 0.01, 0.001]: #probabilidades de error
     print("\n\nEmpezando test con error " + str(e))
     # Calcular el tamaño del arregglo m y la cantidad de funciones k
     m = math.ceil(-D*math.log(e) / (math.log(2)**2))
     k = round( (m/D) * math.log(2))
+    print("m= "+ str(m), "k= " + str(k))
 
     M = bitarray.bitarray(m)
     M.setall(0)
